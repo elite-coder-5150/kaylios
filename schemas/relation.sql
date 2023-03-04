fd4d116c7b1052101406ba03be35fa35d0e0cae0
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-03 20:03:36 -0600

    defined the relation table

--- schemas/relation.sql
+++ schemas/relation.sql
@@ -7,11 +7,3 @@ create table 'relation'
     `date`        timestamp        not null default current_timestamp,
     `time`        timestamp        not null default current_timestamp
 )
-
-#q: can mysql handle a million users?
-## a: yes, but it will be slow
-## would mongodb be better?
-### a: yes, but it will be slow
-
-## q: what type of database can handle 1 million users?
-## a:
