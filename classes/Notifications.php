91d849d1028f9a609019990561f0260299eed010
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-03 19:56:05 -0600

    defined the getNotifications function

--- classes/Notifications.php
+++ classes/Notifications.php
@@ -2,18 +2,49 @@
 
 namespace classes;
 
-use classes\Util;
+use classes\FollowSystem\Follow;
+use classes\Time;
 
-class Notification {
+class Notification
+{
     protected $db;
     protected $util;
-
-    public function __construct() {
+    protected $avatar;
+    protected $time;
+    protected $follow;
+    protected $group;
+    protected $message;
+    public function __construct()
+    {
         $this->db = new Database();
         $this->util = new Util();
+        $this->avatar = new Avatar();
+        $this->time = new Time();
+        $this->follow = new Follow();
+        $this->group = new Group();
+        $this->message = new Message();
+
     }
 
-    public function notifyFollow ($get) {
+    public function notifyCount()
+    {
+        if (isset($_SESSION['id'])) {
+            $session = $_SESSION['id'];
+
+            $sql = "SELECT notify_id FROM notifications WHERE notify_to=:session";
+
+            $query = $this->db->prepare($sql);
+            $query->execute(array(':session' => $session));
+
+            return $query->rowCount();
+
+
+        }
+    }
+
+        public function getNotificatins() {}
+    public function notifyFollow($get)
+    {
         $session = $_SESSION['id'];
 
         $sql = "INSERT INTO notifications (notify_by, notify_to, type, time) VALUES(:session, :get, 'follow', now())";
@@ -22,5 +53,18 @@ class Notification {
         $query->execute(array(':session' => $session, ':get' => $get));
 
         echo "<div class='alert alert-success'>You are now following " . $this->util->getDetails($get, 'username') . "</div>";
+    }
+
+    public function getNotifications($type) {
+        $session = $_SESSION['id'];
+
+        if ($type == "follow") {
+            echo "<li class='notify'>
+                    <div class='profile'>
+                        
+                    </div>
+                 </li>";
+        }
+
     }
 }
