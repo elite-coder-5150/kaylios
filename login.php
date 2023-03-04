2755fd19ebeaf6cc9f8c331a8efb6d58796e0644
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-03 21:01:15 -0600

    defined the structure for the login page

---
+++ login.php
@@ -0,0 +1,40 @@
+<div class="container login-container">
+    <div class="login-card">
+        <header class="card-header">
+            <div class="logo"></div>
+            <h1 class="welcome-title">welcome back</h1>
+            <p class="supportive-text">please enter your details</p>
+        </header>
+        <div class="card-body">
+            <form class="login-form">
+                <div class="input-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Enter your email">
+                </div>
+                <div class="input-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Enter your password">
+                </div>
+                <div class="input-group">
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember">Remember me for 30 days</label>
+                    <div class="forgot-password">
+                        <a href="#">Forgot password?</a>
+                    </div>
+                </div>
+                <div class="button-group">
+                    <button class="primary">sign in</button>
+                    <button class="secondary">
+                        <div class="icon">
+                            <div class="google-icon"></div>
+                        </div>
+                        sign in with Google
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="card-footer">
+            <p>dont have an account? <a href="#" class="signup">Sign up</a></p>
+        </div>
+    </div>
+</div>
