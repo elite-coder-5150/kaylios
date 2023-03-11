c0078c0e47722489e0bddb636e25a915cc068010
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-11 15:27:11 -0600

    defined the is following method for the follow system

--- classes/Follow.php
+++ classes/Follow.php
@@ -19,68 +19,22 @@ class Follow {
         $this->settings = new Settings();
     }
 
-
-
-    public function follow($get) {
-        $session = $_SESSION['id'];
-        $settings = new Settings();
-        $notify = new Notification();
-        $util = new Util();
-
-        $session_u = $this->util->getDetails($session, 'username');
-        $get_u = $this->util->getDetails($get, 'username');
-        $this->notify->notifyFollow($get);
-
-        if (!$this->settings->AmIBlocked($get)) {
-            if (!$this->isFollowing($get)) {
-                $sql = "INSERT INTO follow_system (follow_by, follow_to, time) VALUES(:session, :get, now())";
-
-                $query = $this->db->prepare($sql);
-                $query->execute(array(':session' => $session, ':get' => $get));
-
-                $this->notify->notifyFollow($get, "follow");
-            }
-        }
-    }
-
-    public function unfollow($get) {
-        if ($this->isFollowing($get)) {
-            $session = $_SESSION['id'];
-
-            $sql = "DELETE FROM follow_system WHERE follow_by=:session AND follow_to=:get LIMIT 1";
-
-            $query = $this->db->prepare($sql);
-            $query->execute(array(':session' => $session, ':get' => $get));
-
-            // return "ok";
-
-            $this->notify->getNotifications("friend-request", $get, "unfollow");
-        }
-    }
-
-    public function followers($get) {
-        $session = $_SESSION['id'];
-
-        $sql = "SELECT follow_by FROM follow_system WHERE follow_to=:session";
-
-        $query = $this->db->prepare($sql);
-        $query->execute(array(':session' => $session));
-    }
-    
-    public function isFollowing ($get) {
+    public function isFollowing($get) {
         if (isset($_SESSION['id'])) {
             $session = $_SESSION['id'];
 
-            $sql = "SELECT follow_to FROM follow_system WHERE follow_by=:session AND follow_to=:get LIMIT 1";
+            $sql = "SELECT follow_to FROM follow_system
+                    WHERE follow_from = :session 
+                    AND follow_to = :get
+                    LIMIT 1";
 
             $query = $this->db->prepare($sql);
-            $query->execute(array(':session' => $session, ':get' => $get));
+            $query->execute([
+                ':session' => $session,
+                ':get' => $get
+            ]);
 
-            if ($query->rowCount() != 0 || $query->rowCount() != null) {
-                return true;
-            } else if ($query->rowCount() == 0) {
-                return false;
-            }
+            return $query->rowCount() == 1;
         }
     }
 }
