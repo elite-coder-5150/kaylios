3ceb8b1a095ba8764882c7c9485dbbc3b3d7250a
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-12 16:17:13 -0500

    defined the is following method

--- classes/Follow.php
+++ classes/Follow.php
@@ -24,75 +24,18 @@ class Follow {
             $session = $_SESSION['id'];
 
             $sql = "SELECT follow_to FROM follow_system
-                    WHERE follow_from = :session 
-                    AND follow_to = :get
-                    LIMIT 1";
-
-            $query = $this->db->prepare($sql);
-            $query->execute([
-                ':session' => $session,
-                ':get' => $get
-            ]);
-
-            return $query->rowCount() == 1;
-        }
-    }
-
-    public function follow($get) {
-        $session = $_SESSION['id'];
-        $session_u = $this->util->getDetails($session, 'username');
-        $get_u = $this->util->getDetails($get, 'username');
-
-        if (!$this->settings->AmIBlocked($get)) {
-            if (!$this->isFollowing($get)) {
-                $sql = "INSERT INTO follow_system (follow_from, follow_to)
-                        VALUES (:session, :get)
-                    ";
-
-                $query = $this->db->prepare($sql);
-                $query->execute([
-                    ':session' => $session,
-                    ':get' => $get
-                ]);
-
-                $this->notify->notifyFollow($get, $session_u, $get_u);
-                return "ok";
-            } else {
-                return "You are already following this user";
-            }
-        }
-    }
-
-    public function unfollow($get) {
-        if ($this->isFollowing($get)) {
-            $session = $_SESSION['id'];
-
-            $sql = "DELETE FROM follow_system
-                    WHERE follow_by = :session
-                    AND follow_to = :get
+                    WHERE follow_by=:session
+                    AND follow_to=:to,
                     LIMIT 1
                 ";
 
             $query = $this->db->prepare($sql);
             $query->execute([
                 ':session' => $session,
-                ':get' => $get
+                ':to' => $get
             ]);
+
+            return $query->rowCount() != 0 || $query->rowCount() != null;
         }
-    }
-
-    public function getFollowers($get) {
-        $session = $_SESSION['id'];
-
-        $sql = "SELECT follow_by FROM follow_system
-                WHERE follow_to = :get
-            ";
-
-        $query = $this->db->prepare($sql);
-        $query->execute([
-            ':get' => $get
-        ]);
-
-        return $query->rowCount();
     }
 }
