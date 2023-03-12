689de62d442bf553de0c8b50c0f518ca3825b166
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-12 16:19:54 -0500

    defined the am i blocked method

--- classes/Settings.php
+++ classes/Settings.php
@@ -2,9 +2,30 @@
 
 namespace classes;
 
-class Settings
-{
-    public function AmIBlocked ($get) {
+class Settings {
+    protected $db;
 
+    public function __construct() {
+        $this->db = new Database();
     }
+    public function AmIBlocked ($get) {
+        $session = $_SESSION['id'];
+
+        $sql = "SELECT block_id FROM block
+                WHERE block_by=:by
+                AND block_to=:to
+            ";
+
+        $query = $this->db->prepare($sql);
+        $query->execute([
+            ':by' => $session,
+            ':to' => $get
+        ]);
+
+        $count = $query->rowCount();
+
+        return $count == 1;
+    }
+
+
 }
