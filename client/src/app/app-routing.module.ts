98ba52339d875f6456b4b0fa50aaa1d030f72e60
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-02 17:15:59 -0600

    defined the routes for the login, register and user compoennts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+     path: 'register',
+     component: RegisterComponent
+  },
+  {
+    path: 'user/:id',
+      component: UserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
