5408e94b634c08d885e3ef2d5b9369097b57f0e9
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-02 17:29:56 -0600

    imported user-list, edit-user, and add-user components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    UserListComponent,
+    EditUserComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
