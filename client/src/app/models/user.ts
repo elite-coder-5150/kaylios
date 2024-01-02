6734a78a9fb6fb50fff9181847a6d44e944fea09
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-02 17:18:53 -0600

    added the user-roles

--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -1,8 +1,10 @@
+import { UserRoles } from "./user-roles";
 export interface User {
     user_id: string;
     user_name: string;
     email: string;
     pass_hash: string;
+    role: UserRoles;
     bio: string;
     profile_picture_url: string;
     joined_date: Date;
