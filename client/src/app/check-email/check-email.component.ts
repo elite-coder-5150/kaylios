558e1e5a31d1be020226e2258ec605f1eae7e6ba
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-04 16:39:06 -0600

    default logic

---
+++ client/src/app/check-email/check-email.component.ts
@@ -0,0 +1,11 @@
+// TODO: refector this to work properly
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-check-email',
+  templateUrl: './check-email.component.html',
+  styleUrls: ['./check-email.component.scss']
+})
+export class CheckEmailComponent {
+
+}
