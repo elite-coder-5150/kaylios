2f5bf21cdef6d0ececf4d4045ed4c0eecb487f90
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-05 15:50:25 -0600

    defined the routes for the search feature

---
+++ server/routes/search.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+import * as sc from "../controllers/search.controller";
+
+router.get('/search/user/:user_id', sc.searchUsers); 
+router.get('/search/category/:category_id', sc.searchCategory); 
+router.get('/search/tags/:tag_id', sc.searchTags); 
+router.get('/search/groups/:group_id', sc.searchGroups); 
+router.get('/search/videos/:video_id', sc.searchVideos);
+router.get('/search/notes/:notes_id', sc.searchNotes);
+
+module.exports = router;
