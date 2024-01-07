d006b134a5dbf087fceb4b8d51ce7000167dc731
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-07 16:54:16 -0600

    changed the name of the selector, defined the forgotPasswordForm variable

--- client/src/app/forgot-password/forgot-password.component.ts
+++ client/src/app/forgot-password/forgot-password.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
-  selector: 'app-forgot-password',
+  selector: 'ng-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent {
+  forgotPasswordForm: FormGroup;
 
+  constructor (private fb: FormBuilder) {
+    this.forgotPasswordForm = this.fb.group({});
+  }
+
+  onSubmit() {
+    
+  }
 }
