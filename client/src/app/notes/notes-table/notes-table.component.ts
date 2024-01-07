28169c580ea4cd619b682df266ed3769a601449e
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-07 17:06:55 -0600

    lotic for the component

---
+++ client/src/app/notes/notes-table/notes-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-table',
+  templateUrl: './notes-table.component.html',
+  styleUrls: ['./notes-table.component.scss']
+})
+export class NotesTableComponent {
+
+}
