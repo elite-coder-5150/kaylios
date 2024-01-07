e73b51a5a653f3571c9f1a8b8ff2de7fab8d18a9
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-07 17:02:04 -0600

    defined the loginform variable

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = fb.group({});
+  }
+
+  login() {}
 }
