ad3e07a1dd76f7ac0963e565359aba41b7ad2220
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-08 08:24:35 -0600

    defined the routes for the notes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import * as nc from '../controllers//notes.controller';
 
 router.get('/notes', nc.getAllNotesByUser)
-router.get('/notes/:note_id', nc.getSingleNotes);
+router.get('/notes/:note_id', nc.getSingleNote);
 ro8uter.post('/notes/:note_id', nc.createNote);
 router.put('/notes/:note_id', nc.updateNote);
 router.delete('/notes/:note_id', nc.deleteNote);
