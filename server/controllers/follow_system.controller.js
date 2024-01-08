59498abe251534faa3bf50ddc505e2e627eec00b
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-08 08:23:13 -0600

    follow system route handler

---
+++ server/controllers/follow_system.controller.js
@@ -0,0 +1,11 @@
+export const follow = async (req, res) => {};
+
+export const unfollow = async (req, res) => {};
+
+export const getFollowers = async (req, res) => {
+    const htmk_test = /* html */`
+        <div class="">
+            
+        </div>
+    `
+};
