2bbd88ec79575fcfd4558bdaaddea68bd959fe3d
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-08 08:05:15 -0600

    defined the resend method

--- client/src/app/otp-check-email/otp-check-email.component.ts
+++ client/src/app/otp-check-email/otp-check-email.component.ts
@@ -1,5 +1,6 @@
+// TODO: REFACTOR THIS CODE TO WORK PROPERLY.
 import { Component } from '@angular/core';
-
+import { EmailService } from '../services/email.service';
 @Component({
   selector: 'ng-otp-check-email',
   templateUrl: './otp-check-email.component.html',
@@ -8,7 +9,18 @@ import { Component } from '@angular/core';
 export class OtpCheckEmailComponent {
   opt: string = '';
 
+  constructor(private emailService: EmailService) {}
   validateNumericInput()  {
     this.opt = this.opt.replace(/\D/g, '');
+  }
+
+
+  resend() {
+    this.emailService.resendVerificationEmail()
+      .subscribe((response) => {
+        console.log('email resent successfully: ', response);
+      }, (err) => {
+        console.error('Error resending verification email: ', err);
+      })
   }
 }
