7befcc62d4ae165fc34b3f3eeb757fde359c8e58
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-08 15:38:35 -0600

    defined the form data

--- client/src/app/contact-me/contact-me.component.ts
+++ client/src/app/contact-me/contact-me.component.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import axios from 'axios';
-
+import { parse, parsePhoneNumberFromString } from 'libphonenumber-js'
 @Component({
     selector: 'ng-contact-me',
     templateUrl: './contact-me.component.html',
     styleUrls: ['./contact-me.component.scss'],
 })
 export class ContactMeComponent {
-    constructor(public fb: FormBuilder) {
-        this.contactForm = fb.group({});
-    }
     contactForm: FormGroup;
 
-    onSubmit() {}
+    constructor() {
+        this.contactForm = new FormGroup({
+            firstName: new FormControl('', [
+                Validators.required, 
+                Validators.minLength(3), 
+                Validators.maxLength(30)]),
+            lastName: new FormControl('', [
+                Validators.required,
+                Validators.minLength(3),
+                Validators.maxLength(30)]),
+            email: new FormControl('', [
+                Validators.required,
+                Validators.email,
+                Validators.minLength(3),
+                Validators.maxLength(30)]),
+            phoneNumber: new FormControl('', [
+                Validators.required
+            ])
+        });
+    }
+
+    
+
+    onSubmit() {
+        const firstName = this.contactForm.get('firstName')?.value;
+        const lastName = this.contactForm.get('lastName')?.value;
+        const email = this.contactForm.get('email')?.value;
+        const phoneNumber = this.contactForm.get('phoneNumber')?.value;
+        const parsedPhoneNumber = parsePhoneNumberFromString(phoneNumber, 'US');
+
+        
+
+    }
 }
