4238c70fb2cdc847162779ee6a16b9940af815bb
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-08 08:23:43 -0600

    search route handler

---
+++ server/controllers/search.controller.js
@@ -0,0 +1,11 @@
+export const searchUsers = async (req, res) => {};
+
+export const searchCategory = async (req, res) => {};
+
+export const searchTags = async (req, res) => {};
+
+export const searchNotes = async (req, res) => {};
+
+export const searchGroups = async (req, res) => {};
+
+export const searchVideos = async (req, res) => {};
