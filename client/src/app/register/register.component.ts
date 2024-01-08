0309d326c25fe2caf850c669100a2a6e342b3dc0
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-08 08:16:58 -0600

    defined the register form variable

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
-  selector: 'app-register',
+  selector: 'ng-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = fb.group({});
+  }
+  register() {
+    
+  }
 }
