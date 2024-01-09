0362475505575c8d0d373990497eb45939daaf66
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-09 12:50:52 -0600

    defined the subscribe method

---
+++ client/src/app/services/newsletter.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsletterService {
+
+  constructor() { }
+
+  // subscribe(email:string): Observable {}
+}
