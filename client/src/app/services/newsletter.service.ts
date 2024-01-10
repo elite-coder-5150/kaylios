82ff35dd59ff2e6304097e480c3178f489db5d5f
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-10 12:48:23 -0600

    defined the subscribe method

--- client/src/app/services/newsletter.service.ts
+++ client/src/app/services/newsletter.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { NewsletterFormData } from '../models/newsletter-form-data'
+import axios from 'axios';
 @Injectable({
   providedIn: 'root'
 })
 export class NewsletterService {
-
+  private apiUrl = "http://localhost:3000/newsletter";
   constructor() { }
 
-  subscribe(email:string) {}
+  // TODO: use promises not observables
+  subscribe(newsletterFormData: NewsletterFormData) {}
 }
