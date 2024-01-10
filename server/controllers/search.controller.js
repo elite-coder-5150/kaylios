c7b5e12fdb28f00d9953ca699738936240cb26e6
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-10 14:28:55 -0600

    defined the searchUser route handler

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -1,4 +1,33 @@
-export const searchUsers = async (req, res) => {};
+import { getResults } from '../utility/getResults';
+
+export const searchUsers = async (req, res) => {
+    try {
+        const searchTerm = req.query.searchTerm;
+
+        const sql = /* sql */`
+            select * from users where username LIKE ?
+        `;
+
+        const users = await getResults(sql, ['%${searchTerm}%']);
+
+        if (users.length === 0) {
+            return res.status(404).send({
+                message: 'User not found'
+            });
+        }
+
+        return res.status(200).send({
+            message: 'User successfully found',
+            data: users
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).send({
+            error: 'Internal Server Error'
+        });
+    }
+};
 
 export const searchCategory = async (req, res) => {};
 
