688d7dfdb7776186cd1a2bd4cbb70ad9716a2390
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-10 11:19:54 -0600

    defined the subscribe method for the newsletter component

--- client/src/app/newsletter/newsletter.component.ts
+++ client/src/app/newsletter/newsletter.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { NewsletterService } from '../services/newsletter.service';
+import { NewsletterFormData } from '../models/newsletter-form-data';
+import axios from 'axios';
 @Component({
   selector: 'ng-newsletter',
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.scss']
 })
 export class NewsletterComponent {
-  email: string = '';
+  private apiUrl = "http://localhost:3000/newsletter";
 
   constructor(private nls: NewsletterService) {}
 
-  // subscribe() {
-  //   this.nls.subscribe(this.email)
-  //     .subscribe(response => {
-  //       console.log('', response);
-  //     }, err => {})
-  // }
+  subscribe(newsletterFormData: NewsletterFormData): void {
+    axios.post(this.apiUrl, newsletterFormData)
+      .then(response => {
+        console.log('data sent successfully', response.data);
+      }).catch(err => {
+        console.error('error sending data', err);
+      });
+  }
 }
