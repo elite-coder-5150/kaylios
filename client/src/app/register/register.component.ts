e1315e15d714fe846fd56ad9a5f1a1ec8e6645ed
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-15 19:09:53 -0600

    checked to see if the passwords match

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { FormDataBase } from '../models/simple-form-data';
 @Component({
   selector: 'ng-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  registerForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.registerForm = fb.group({});
+  registerForm: any = {
+    username: null,
+    email: null,
+    password: null,
   }
-  register() {
-    
+
+  formData: FormDataBase = {
+    isSuccessful: false,
+    signupFailed: false, 
+    loginFailed: false,
+    errorMsg: ''
+  }
+
+  constructor(private authService: AuthService) {
+   
+  }
+  onSubmit() {
+    const { username, email, password, confPass } = this.registerForm;
+
+    if (password.value !== confPass.value) {
+      this.formData.errorMsg = "I'm sorry but the password do not match";
+      this.formData.isSuccessful = false;
+    }
+
+    if (this.registerForm.valid) {
+      this.authService.register(username, email, password).subscribe({
+        next: data => {
+          console.log(data);
+
+          this.formData.isSuccessful = true;
+          this.formData.signupFailed = false;
+        },
+        error: err => {
+          this.formData.errorMsg = err.error.message;
+          this.formData.isSuccessful = false;
+        }
+      })
+    }
   }
 }
