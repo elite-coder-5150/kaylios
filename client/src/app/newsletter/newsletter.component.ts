46c9597292f22682bb180ac70d597dbb5768549d
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-15 19:12:52 -0600

    defined the onSubmit

--- client/src/app/newsletter/newsletter.component.ts
+++ client/src/app/newsletter/newsletter.component.ts
@@ -1,29 +1,49 @@
 import { Component } from '@angular/core';
 import { NewsletterService } from '../services/newsletter.service';
 import { NewsletterFormData } from '../models/newsletter-form-data';
-import axios from 'axios';
+import { FormDataBase } from '../models/simple-form-data';
 @Component({
   selector: 'ng-newsletter',
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.scss']
 })
 export class NewsletterComponent {
-  showSuccessMessage: boolean = false;
+
+  public form: any = {
+    userName: null,
+    email: null,
+  }
+
+  public isSuccessful: boolean = false;
+  public signUpFailed: boolean = false;
+  public errorMsg: string = '';
+
+  newsletterFormData: NewsletterFormData = {
+    name: '',
+    email: '',
+    subscribeDate: new Date(),
+    isSuccessful: false,
+    subscriptionFailed: false,
+    errorMsg: ''
+
+  }
 
   constructor(private nls: NewsletterService) {}
 
-  onSubmit(newsletterFormData:NewsletterFormData) {
-   this.nls.subscribe(newsletterFormData)
-    .subscribe(data => {
-      console.log('form data sent successfully', data);
+  onSubmit() {
+    const { userName, email } = this.form;
 
-      this.showSuccessMessage = true;
+    this.nls.subscribe(userName, email).subscribe({
+      next: data => {
+        console.log(data);
 
-      setTimeout(() => {
-        this.showSuccessMessage = false
-      }, 3000);
-    }, error => {
-      console.log('error submitting form data', error);
+        this.newsletterFormData.isSuccessful = true;
+        this.newsletterFormData.subscriptionFailed = false;
+      },
+      error: err => {
+        this.newsletterFormData.errorMsg = err.error.message;
+        this.newsletterFormData.subscriptionFailed = true;
+      }
     })
   }
   
