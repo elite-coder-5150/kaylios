f1c079a31d33f32a2a7a9cdc75041738643fb1f5
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-16 10:00:23 -0600

    defined the unsubscribe route handler

--- server/controllers/newsletter.controller.js
+++ server/controllers/newsletter.controller.js
@@ -1,15 +1,15 @@
-import { getResults } from "../utility/getResults";
-
-export const createNewsletter = async  (req, res) => {
+import { Utility as utility } from "../utility/getResults";
+import { sendEmails } from "../utility/send-emails";
+export const subscribe = async  (req, res) => {
     try {
-        const { email, joined_date } = req.body;
+        const { name, email, joined_date } = req.body;
 
         const sql = /* sql */`
-            insert into newsletter (email, joined_date)
-            values (?, ?)
+            insert into newsletter (name, email, joined_date)
+            values (?, ?, ?);
         `;
 
-        const result = await getResults(sql, [email, joined_date]);
+        const result = await utility.getResults(sql, [name, email, joined_date]);
 
         if (!result) {
             return res.status(400).send({
@@ -30,6 +30,48 @@ export const createNewsletter = async  (req, res) => {
     }
 };
 
-export const unsubscribe = async (req, res) => {};
+export const unsubscribe = async (req, res) => {
+    try {
+        const { name, email } = req.body;
 
-export const bulkSendEmail = async (req, res) => {};
+        const sql = /* sql */`
+            delete from newsletter where name = ? and email = ?
+        `;
+
+        const result = await utility.getResults(sql, [name, email]);
+
+        return res.status(200).send({
+            message: 'successfully unsubscribed from the mailing list',
+            data: result
+        })
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).send({
+            message: 'internal server error'
+        })
+    }
+};
+
+// this is my code not AI generated.
+// export const bulkSendEmail = async (req, res) => {
+//     try {
+//         const { emailAddr } = req.body;
+
+//         const sql = /* sql */`
+//             select * from newsletter
+//         `;
+
+//         const emails = await utility.getResults(sql, [emailAddr]);
+
+//         emails.forEach((email) => {
+//             sendEmails(email);
+//         })
+//     } catch (error) {
+//         console.error(error);
+
+//         res.status(500).send({
+//             message: 'internal server error'
+//         })
+//     }
+// };
