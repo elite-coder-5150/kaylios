eb9657f6e496f05b194c360d5729cd767e38a842
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-16 14:06:58 -0600

    defined the getUsers method

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('/api/users');
+  }
 }
