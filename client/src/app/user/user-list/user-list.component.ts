71c738c8f38b43744dc8038c69b59e1c968e8021
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-16 14:05:43 -0600

    implemented the get users method from the user service

--- client/src/app/user/user-list/user-list.component.ts
+++ client/src/app/user/user-list/user-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 @Component({
-  selector: 'app-user-list',
+  selector: 'ng-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
-
+export class UserListComponent implements OnInit {
+  users: any;
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
 }
