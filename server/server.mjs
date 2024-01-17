20e6a9ca4dd0b026a5c0d8841109a6d920e963d1
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-17 11:35:30 -0600

    got the server and the database to work

---
+++ server/server.mjs
@@ -0,0 +1,36 @@
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+
+// const express = require('express');
+import express from 'express';
+// const mysql = require('mysql');
+import mysql from 'mysql2'
+const app = express();
+// const bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
+
+const port = 3000;
+
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+})
+export const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    port: 8889,
+    database: 'kaylios',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('error connecting to the database', err);
+        return;
+    }
+    console.log('connected to the datbase');
+});
+
