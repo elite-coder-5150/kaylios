8a69aaa5ace5f814dafd868944cd026858df99ce
elite-coder-5150 <parkhousedarrell7@gmail.com>
2024-01-29 02:56:12 -0600

    used the route handler from the controllers

--- server/routes/notes.mjs
+++ server/routes/notes.mjs
@@ -1,11 +1,24 @@
-const express = require('express');
+import  express from 'express';
 const router = express.Router();
-import * as nc from '../controllers/notes.controller';
+import { Utility as util } from '../utility/utility.mjs';
 
-router.get('notes', nc.getAllNotesByUser)
-router.get('notes/:note_id', nc.getSingleNote);
-ro8uter.post('notes/:note_id', nc.createNote);
-router.put('notes/:note_id', nc.updateNote);
-router.delete('notes/:note_id', nc.deleteNote);
+import { 
+    getAllNotesByUser,
+    getSingleNote,
+    createNote,
+    updateNote,
+    deleteNote,
+} from '../controllers/notes.controller';
+
+router.get('/api/notes', getAllNotesByUser);
+
+
+router.get('/api/notes/:note_id', getSingleNote);
+// ro8uter.post('notes/:note_id', nc.createNote);
+router.post('/api/notes/:note_id', createNote);
+// router.put('notes/:note_id', nc.updateNote);
+router.put('/api/notes/:note_id', updateNote);
+// router.delete('notes/:note_id', nc.deleteNote);
+router.delete('/api/notes/:note_id', deleteNote);
 
 module.exports = router;
